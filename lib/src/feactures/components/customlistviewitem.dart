a4b7ea54613cbef4949809e49b550414484cbf24
Fabio dos Santos <programadornatal@gmail.com>
2023-08-24 01:31:05 -0300

    Create list with fixed data and search working

--- lib/src/feactures/components/customlistviewitem.dart
+++ lib/src/feactures/components/customlistviewitem.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 
 class Customlistviewitem extends StatelessWidget {
   final String title;
-  const Customlistviewitem({super.key, required this.title});
+  final String data;
+  const Customlistviewitem(
+      {super.key, required this.title, required this.data});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         title: Text(title),
-        subtitle: const Text('Testando super texto'),
+        subtitle: Text(data),
         leading: ConstrainedBox(
             constraints: const BoxConstraints(
                 minHeight: 44, minWidth: 44, maxHeight: 64, maxWidth: 64),
